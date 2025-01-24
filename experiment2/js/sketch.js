ed5da718793967955b7c51ff81adf84ef1a42651
scso-ucsc <scso@ucsc.edu>
2025-01-23 21:39:17 -0800

    Adding Experiment 2 Content

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -1,30 +1,34 @@
 // sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Sean Eric So
+// Date: January 23, 2025
 
 // Here is how you might set up an OOP p5.js project
 // Note that p5.js looks for a file called sketch.js
 
 // Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
+var NORTH = 0;
+var NORTHEAST = 1;
+var EAST = 2;
+var SOUTHEAST = 3;
+var SOUTH = 4;
+var SOUTHWEST = 5;
+var WEST = 6;
+var NORTHWEST = 7;
+var direction;
+
+var stepSize = 20;
+var posX, posY;
+var diameter = 20;
+
+let colourChangeSound, dotSound, blockSound, snowflakeSound;
+let shapeStyle;
+let isPaused = false;
 
 // Globals
 let myInstance;
 let canvasContainer;
 var centerHorz, centerVert;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
+var canvasWidth, canvasHeight;
 
 function resizeScreen() {
   centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
@@ -32,6 +36,13 @@ function resizeScreen() {
   console.log("Resizing...");
   resizeCanvas(canvasContainer.width(), canvasContainer.height());
   // redrawCanvas(); // Redraw everything based on new size
+}
+
+function preload(){
+  colourChangeSound = loadSound("../audio/Ice1.mp3");
+  dotSound = loadSound("../audio/pickupCoin.wav");
+  blockSound = loadSound("../audio/hitHurt.wav");
+  snowflakeSound = loadSound("../audio/click.wav");
 }
 
 // setup() function is called once when the program starts
@@ -42,38 +53,118 @@ function setup() {
   canvas.parent("canvas-container");
   // resize canvas is the page is resized
 
-  // create an instance of the class
-  myInstance = new MyClass("VALUE1", "VALUE2");
+  resizeScreen();
 
   $(window).resize(function() {
     resizeScreen();
   });
-  resizeScreen();
-}
+  canvasWidth = canvasContainer.width();
+  canvasHeight = canvasContainer.height();
 
-// draw() function is called repeatedly, it's the main animation loop
-function draw() {
-  background(220);    
-  // call a method on the instance
-  myInstance.myMethod();
-
-  // Set up rotation for the rectangle
-  push(); // Save the current drawing context
-  translate(centerHorz, centerVert); // Move the origin to the rectangle's center
-  rotate(frameCount / 100.0); // Rotate by frameCount to animate the rotation
-  fill(234, 31, 81);
   noStroke();
-  rect(-125, -125, 250, 250); // Draw the rectangle centered on the new origin
-  pop(); // Restore the original drawing context
-
-  // The text is not affected by the translate and rotate
-  fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", centerHorz - 105, centerVert + 40);
+  fill(0, 0, 0, 100);
+  
+  posX = canvasWidth / 2;
+  posY = canvasHeight / 2;
+  
+  shapeStyle = 0;
+  isPaused = false;
+  
+  background(240, 240, 240);
+  
+  setInterval(playSound, 200);
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+function draw() {
+  if(isPaused == false){
+    direction = int(random(0, 8));
+
+    if(direction == NORTH){
+      posY -= stepSize;
+    } else if(direction == NORTHEAST){
+      posY -= stepSize;
+      posX += stepSize;
+    } else if(direction == EAST){
+      posX += stepSize;
+    } else if(direction == SOUTHEAST){
+      posY += stepSize;
+      posX += stepSize;
+    } else if(direction == SOUTH){
+      posY += stepSize;
+    } else if(direction == SOUTHWEST){
+      posY += stepSize;
+      posX -= stepSize;
+    } else if(direction == WEST){
+      posX -= stepSize;
+    } else if(direction == NORTHWEST){
+      posY -= stepSize;
+      posX -= stepSize;
+    }
+
+    //Considerations for off canvas
+    if(posX > canvasWidth) posX = 0;
+    if(posX < 0) posX = canvasWidth;
+    if(posY > canvasHeight) posY = 0;
+    if(posY < 0) posY = canvasHeight;
+
+    //Drawing unique shape
+    translate(posX, posY);
+
+    if(shapeStyle == 0){
+      ellipse(0, 0, diameter, diameter);
+    } else if(shapeStyle == 1){
+      rect(-diameter / 2, -diameter / 2, diameter, diameter)
+    } else if(shapeStyle == 2){
+      beginShape();
+      for(let angle = 0; angle < TWO_PI; angle += 0.1){
+        let radius = 1 + sin(angle * 5) * 10 + random(-5, 5);
+        let x = radius * cos(angle);
+        let y = radius * sin(angle);
+        vertex(x, y);
+      }
+      endShape(CLOSE); 
+    }
+  }
+}
+
+function mousePressed(){
+  colourChangeSound.play();
+  let r = random(255);
+  let g = random(255);
+  let b = random(255);
+  fill(r, g, b, 100);
+  posX = mouseX;
+  posY = mouseY;
+}
+
+function keyReleased(){
+  if(keyCode == 32){
+    setPauseOrPlay();
+  } else if(keyCode == 49){
+    shapeStyle = 0;
+  } else if(keyCode == 50){
+    shapeStyle = 1;
+  } else if(keyCode == 51){
+    shapeStyle = 2;
+  }
+}
+
+function playSound(){
+  if(isPaused == false){
+    if(shapeStyle == 0){
+    dotSound.play();
+    } else if(shapeStyle == 1){
+      blockSound.play();
+    } else if(shapeStyle == 2){
+      snowflakeSound.play(); 
+    }
+  }
+}
+
+function setPauseOrPlay(){
+  if(isPaused == true){
+    isPaused = false;
+  } else{
+    isPaused = true;
+  }
 }
