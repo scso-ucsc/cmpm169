530b93c15fc1890acebdb18ee7f0a4ab0d58f1ae
scso-ucsc <scso@ucsc.edu>
2025-02-22 01:39:05 -0800

    Updating Experiment 6 Information

--- experiment6/js/sketch.js
+++ experiment6/js/sketch.js
@@ -1,6 +1,6 @@
 // sketch.js - purpose and description here
 // Author: Sean Eric So
-// Date: 15 February, 2025
+// Date: 22 February, 2025
 
 // Here is how you might set up an OOP p5.js project
 // Note that p5.js looks for a file called sketch.js
